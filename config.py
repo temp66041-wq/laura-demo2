e861a824bfee7994140b913d8fcbfd7f0f822133
temp66041-wq <temp66041@gmail.com>
2026-03-11 14:39:56 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+# Example configuration file with a secret
+ 
+API_KEY = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+DATABASE_PASSWORD = "SuperSecretPassword123"
+ 
+def connect():
+    print("Connecting with secret credentials...")
